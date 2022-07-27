492a6a5fc43e16aef4c0524d7922115a853a80ae
Asadullah khan <aukhan@akru.co>
2022-07-27 16:51:35 +0500

    ok

--- Tenant/Deposite.py
+++ Tenant/Deposite.py
@@ -22,27 +22,28 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from yopmail_login import yopmail
 import variables
 from chrome_setup import chrome
+from combine import combine_allure
 
 
 class PythonOrgSearch(unittest.TestCase):
     
     def setUp(self):
-        # WINDOW_SIZE = "1920,1080"
-        # chrome_options = Options()
-        # #chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument('--start-maximized')
-        # chrome_options.add_argument('--disable-setuid-sandbox')
-        # #s = Service('/home/ubuntu/script/pipeline/test/chromdriver/chromedriver')
-        # # s = Service('/Users/qualityassurance/Desktop/automation-scripts/AVAXDEV_SHAHWAR/chromedriver')
+        WINDOW_SIZE = "1920,1080"
+        chrome_options = Options()
+        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--start-maximized')
+        chrome_options.add_argument('--disable-setuid-sandbox')
+        #s = Service('/home/ubuntu/script/pipeline/test/chromdriver/chromedriver')
+        # s = Service('/Users/qualityassurance/Desktop/automation-scripts/AVAXDEV_SHAHWAR/chromedriver')
         
         
-        # service = ChromeService(executable_path=ChromeDriverManager().install())
-        # self.driver = webdriver.Chrome(service=service , options=chrome_options)
-       # PATH = "chromedriver"
-        #self.driver = webdriver.Chrome(PATH, options=chrome_options)
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service , options=chrome_options)
+        PATH = "chromedriver"
+        self.driver = webdriver.Chrome(PATH, options=chrome_options)
 
         csk = chrome()
         self.driver = csk.get_driver()
@@ -313,6 +314,7 @@ class PythonOrgSearch(unittest.TestCase):
     def tearDown(self):
         time.sleep(3)
         self.driver.save_screenshot("entsig.PNG")
+        #combine_allure("/Users/asadullahkhan/Desktop/AKRU-Scripts/Property_owners/target/allure-results")
         #allure.attach.file(r"entsig.PNG", "screenshot",attachment_type=allure.attachment_type.PNG)
         time.sleep(3)
         self.driver.quit()
